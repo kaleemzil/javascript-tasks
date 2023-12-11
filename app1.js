ea18fe2ccdf1390699c39d3073f5dfc62941c087
M. KALEEMULLAH KHAN QURESHI <136243025+kaleemzil@users.noreply.github.com>
2023-12-11 11:44:17 -0800

    TASK 2
    ADDED TASK 2 IN THIS WE CHECK THE WORD IS PALINDROME OR NOT

--- app1.js
+++ app1.js
@@ -1,3 +1,5 @@
+//task 3
+
 var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 var evenArray = [];
 var oddArray = [];
@@ -14,4 +16,23 @@ for (var i = 0; i < numberArray.length; i++) {
 console.log("Even Numbers: " + evenArray);
 console.log("Odd Numbers: " + oddArray);
 
+//task 2
 
+let palindrome = prompt("enter the palindrome")
+let flag = true;
+
+
+const len = palindrome.length;
+
+for (let i = 0; i <  len / 2; i++) {
+    if (palindrome[i] !== palindrome[len - i - 1]) {
+        flag = false;
+        break;
+    }
+}
+if (flag) {
+    console.log(`${(palindrome)}: word is  a palindrome`)
+}
+else {
+    console.log(`${(palindrome)}:word is not a palindrome`)
+}
