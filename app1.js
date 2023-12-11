4deab7325b05b224f9b561a14da9777d0b646643
M. KALEEMULLAH KHAN QURESHI <136243025+kaleemzil@users.noreply.github.com>
2023-12-11 11:25:58 -0800

    task no 3
    sorting the even and odd number from an array lenght by using module operator and for loop and also if  else statment

---
+++ app1.js
@@ -0,0 +1,17 @@
+var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+var evenArray = [];
+var oddArray = [];
+
+for (var i = 0; i < numberArray.length; i++) {
+    if (numberArray[i] % 2 === 0) {
+        evenArray.push(numberArray[i]);
+    } else {
+        oddArray.push(numberArray[i]);
+    }
+}
+
+// Display the results in the browser console
+console.log("Even Numbers: " + evenArray);
+console.log("Odd Numbers: " + oddArray);
+
+
